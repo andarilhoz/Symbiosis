1f324a8a4bf9b0e8f76490adefb42f63620bc9f8
Magno Gouveia PC <owlflee@gmail.com>
2016-03-30 01:23:36 -0300

    Adicionado mecanica de pulo

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,13 +4,53 @@ using System.Collections;
 public class PlayerManager : MonoBehaviour {
     public int speedPlayer;
     public Rigidbody2D playerRigibody;
-	// Use this for initialization
-	void Start () {
+    public int jumpForce;
+    public LayerMask whatIsGround;
+    public float groundCheckRadious;
+    public Transform groundCheck;
+    private bool grounded;
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+    private bool stoppedJumping;
+
+
+    // Use this for initialization
+    void Start () {
         playerRigibody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadious, whatIsGround);
         playerRigibody.velocity = new Vector2(speedPlayer, playerRigibody.velocity.y);
-	}
+        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded) {
+                playerRigibody.velocity = new Vector2(playerRigibody.velocity.x, jumpForce);
+                stoppedJumping = false;
+            }            
+        }
+
+        if ((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && !stoppedJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                playerRigibody.velocity = new Vector2(playerRigibody.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+    }
 }
