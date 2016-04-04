9a160bc5826030047c60fb338d67dbd355c90b11
Magno Gouveia PC <owlflee@gmail.com>
2016-04-04 00:53:05 -0300

    Adicionado TimeScore e DistanciaScore

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public Text scoreText;
+
+    private float scoreCount;
+
+    public Transform playerLocation;
+
+    public Text scoreMeters;
+
+    public float pointsPerSecond;
+
+    public bool scoreIncreasing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (scoreIncreasing) {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+        }
+
+
+
+        scoreText.text = Mathf.Round(scoreCount) + "pts";
+        scoreMeters.text = Mathf.Round(playerLocation.transform.position.x) + "m";
+	}
+}
